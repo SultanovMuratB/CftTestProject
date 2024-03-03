0ba0bd5f15751ffebb025a304487d90fddd6711e
SultanovMuratB <137820590+SultanovMuratB@users.noreply.github.com>
2024-03-03 17:49:38 +0300

    First commit

--- app/src/main/java/com/sultanov/cfttestproject/data/mapper/LocationMapper.kt
+++ app/src/main/java/com/sultanov/cfttestproject/data/mapper/LocationMapper.kt
@@ -5,27 +5,27 @@ import com.sultanov.cfttestproject.domain.item.Location
 
 class LocationMapper {
 
-    fun mapLocationToDto(location: Location): LocationDto {
-        return LocationDto(
-            street = StreetMapper().mapStreetToDto(location.street),
-            city = location.city,
-            state = location.state,
-            country = location.country,
-            postcode = location.postcode,
-            coordinates = CoordinatesMapper().mapCoordinatesToDto(location.coordinates),
-            timezone = TimezoneMapper().mapTimezoneToDto(location.timezone),
-        )
-    }
-
-    fun mapDtoToLocation(locationDto: LocationDto): Location {
-        return Location(
-            street = StreetMapper().mapDtoToStreet(locationDto.street),
-            city = locationDto.city,
-            state = locationDto.state,
-            country = locationDto.country,
-            postcode = locationDto.postcode,
-            coordinates = CoordinatesMapper().mapDtoToCoordinates(locationDto.coordinates),
-            timezone = TimezoneMapper().mapDtoToTimezone(locationDto.timezone),
-        )
-    }
+//    fun mapLocationToDto(location: Location): LocationDto {
+//        return LocationDto(
+//            street = StreetMapper().mapStreetToDto(location.street),
+//            city = location.city,
+//            state = location.state,
+//            country = location.country,
+//            postcode = location.postcode,
+//            coordinates = CoordinatesMapper().mapCoordinatesToDto(location.coordinates),
+//            timezone = TimezoneMapper().mapTimezoneToDto(location.timezone),
+//        )
+//    }
+//
+//    fun mapDtoToLocation(locationDto: LocationDto): Location {
+//        return Location(
+//            street = StreetMapper().mapDtoToStreet(locationDto.street),
+//            city = locationDto.city,
+//            state = locationDto.state,
+//            country = locationDto.country,
+//            postcode = locationDto.postcode,
+//            coordinates = CoordinatesMapper().mapDtoToCoordinates(locationDto.coordinates),
+//            timezone = TimezoneMapper().mapDtoToTimezone(locationDto.timezone),
+//        )
+//    }
 }
